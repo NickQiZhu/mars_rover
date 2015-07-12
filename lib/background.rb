78693149bd283e620daf8c85aadc2f27332440ec
Nick Zhu <nick.qi.zhu@gmail.com>
2015-07-12 12:52:39 -0400

    refator to improve background image readability

--- lib/background.rb
+++ lib/background.rb
@@ -9,10 +9,7 @@ class Background < VisualElement
   end
 
   def draw_image
-    image_width = @image.width
-    image_height = @image.height
-
-    if image_width >= @game.width && image_height >= @game.height
+    if no_tiling_required?(image_height, image_width)
       @image.draw(0, 0, z_index)
     else
       repeat_y = @game.height / image_height
@@ -24,5 +21,19 @@ class Background < VisualElement
         end
       end
     end
+  end
+
+  private
+
+  def no_tiling_required?(image_height, image_width)
+    image_width >= @game.width && image_height >= @game.height
+  end
+
+  def image_height
+    @image.height
+  end
+
+  def image_width
+    @image.width
   end
 end
