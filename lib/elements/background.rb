51de888d58bbd11b81407c63d7924c49f921742d
Nick Zhu <nick.qi.zhu@gmail.com>
2015-09-07 02:50:23 -0400

    [nz] refactored background class

--- lib/elements/background.rb
+++ lib/elements/background.rb
@@ -19,25 +19,18 @@ class Background < VisualElement
   end
 
   def draw_image
-    if no_tiling_required?(image_height, image_width)
-      @image.draw(0, 0, z_index)
-    else
-      repeat_y = (@game.height.to_f / image_height).ceil
-      repeat_x = (@game.width.to_f / image_width).ceil
+    repeat_y = (@game.height.to_f / image_height).ceil
+    repeat_x = (@game.width.to_f / image_width).ceil
 
-      repeat_y.times do |y_offset|
-        repeat_x.times do |x_offset|
-          @image.draw(x_offset * image_width, y_offset * image_height, z_index)
-        end
+    repeat_y.times do |y_offset|
+      repeat_x.times do |x_offset|
+        @image.draw(x_offset * image_width, y_offset * image_height, z_index)
       end
     end
   end
 
   private
 
-  def no_tiling_required?(image_height, image_width)
-    image_width >= @game.width && image_height >= @game.height
-  end
 
   def image_height
     @image.height
@@ -46,4 +39,5 @@ class Background < VisualElement
   def image_width
     @image.width
   end
+
 end
